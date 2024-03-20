41ada9dd51ed63a8da40dfd30234c7e3c87163ad
이재홍 <navihong98@naver.com>
2024-03-20 11:55:23 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/limbus/LimbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
