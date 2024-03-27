c8a612fa3dd2382abc614b6203e7d421bba9520d
compasstar <woohong24@naver.com>
2024-03-27 11:34:06 +0900

    test: PostService 테스트작성

---
+++ src/test/java/com/limbus/api/service/PostServiceTest.java
@@ -0,0 +1,42 @@
+package com.limbus.api.service;
+
+import com.limbus.api.domain.Post;
+import com.limbus.api.repository.PostRepository;
+import com.limbus.api.request.PostCreate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    PostService postService;
+
+    @Test
+    @DisplayName("글작성")
+    void test1() {
+        //given
+        PostCreate postCreate = PostCreate.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+
+        //when
+        postService.write(postCreate);
+
+        //then
+        assertEquals(1L, postRepository.count());
+        Post post = postRepository.findAll().get(0);
+        assertEquals("제목입니다.", post.getTitle());
+        assertEquals("내용입니다.", post.getContent());
+    }
+
+}
