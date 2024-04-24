dae38834bcf80cd47701ebd4f7a8a7dc62df64af
compasstar <woohong24@naver.com>
2024-04-24 15:46:54 +0900

    feat: Vue 생성

---
+++ front/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/write">글 작성</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+</style>
---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import axios from 'axios';
+
+const title = ref("")
+const content = ref("")
+
+const write = function() {
+    axios.get("http://google.com")
+}
+</script>
+
+<template>
+    <div>
+        <el-input v-model="title" placeholder="제목을 입력해주세요" />
+    </div>
+
+    <div>
+        <el-input v-model="content" type="textarea" rows="15"/>
+    </div>
+
+    <div class="mt-2">
+        <el-button type="primary" @click="write()">글 작성완료</el-button>
+    </div>
+</template>
+
+<style>
+
+</style>
