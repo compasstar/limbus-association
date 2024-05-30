5e56c0dbdaa753e8b91413fcdf1b899224ba97f1
compasstar <woohong24@naver.com>
2024-05-30 15:48:42 +0900

    chore: gitignore 적용

--- src/main/java/com/limbus/api/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.limbus.api.repository;
-
-import com.limbus.api.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
-}
